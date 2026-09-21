c3aa4948e90904fa2c21f42e1ec10bfe0484796c
aluno <aluno@lab01-30>
2026-09-21 16:31:35 -0300

    depenencias

---
+++ m/src/main/java/com/mycompany/m/util/HibernateUtil.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.m.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author aluno
+ */
+public class HibernateUtil {
+    private static final SessionFactory factory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() 
+    try{
+        return new Configuration().configure().buildSessionFactory();
+    }   catch(Throwable erro) {
+        throw new ExceptionInInitializerError(erro);
+    }
+}
+
+public static SessionFactory getSessionFactory() {
+    return factory;
+}
+public static void shutdown() {
+    factory.close();
+}
